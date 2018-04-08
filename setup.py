22b38136703c0baaedb053d6869f7b0f40290243
Logan Hennessy <loganjhennessy@gmail.com>
2018-04-07 18:35:00 -0700

    Add setup, license, and project requirements

---
+++ setup.py
@@ -0,0 +1,21 @@
+from os import path
+from setuptools import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="getzipcodes",
+    version="0.1.0",
+    description="Command line utility to get zip codes for"
+                "a given city, state combination.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/loganjhennessy/get-zip-codes",
+    author="Logan Hennessy",
+    author_email="loganjhennessy@gmail.com",
+    license="MIT"
+)
